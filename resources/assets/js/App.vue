17840867962ff2f2d643aa48a04ce14e6d3f1d98
chill kwong <chillkwong@gmail.com>
2017-08-18 18:13:20 +0800

    recipe

---
+++ resources/assets/js/App.vue
@@ -0,0 +1,69 @@
+<template>
+	<div class="container">
+		<div class="navbar">
+			<div class="navbar__brand">
+				<router-link to="/">Recipe Box</router-link>
+			</div>
+			<ul class="navbar__list">
+				<li class="navbar__item" v-if="!checkLogIn">
+					<router-link to="/login">Login</router-link>
+				</li>
+				<li class="navbar__item" v-if="!checkLogIn">
+					<router-link to="/register">Register</router-link>
+				</li>
+				<li class="navbar__item" v-if="checkLogIn">
+					<!-- <router-link to="/logout">Logout</router-link> -->
+					<router-link to="/recipes/create">Create Recipe</router-link>
+				</li>
+				<li class="navbar__item" v-if="checkLogIn">
+					<!-- <router-link to="/logout">Logout</router-link> -->
+					<a @click.stop="logout">Logout</a>
+				</li>
+			</ul>
+		</div>
+		<div class="flash flash__success" v-if="flash.success">
+			{{flash.success}}
+		</div>
+		<div class="flash flash__error" v-if="flash.error">
+			{{flash.error}}
+		</div>
+		<router-view></router-view>
+	</div>
+</template>
+
+<script type="text/javascript">
+	import Flash from './helpers/flash'
+	import Auth from './store/auth'
+	import {post} from './helpers/api'
+	export default {
+		created(){
+			Auth.initialize()
+		},
+		data(){
+			return {
+				flash: Flash.state,
+				auth: Auth.state
+			}
+		},
+		computed: {
+			checkLogIn(){
+				if (this.auth.api_token && this.auth.user_id) {
+					return true
+				}
+				return false
+			}
+		},
+		methods: {
+			logout(){
+				post(`/api/logout`)
+				.then((res)=>{
+					if (res.data.logged_out) {
+						Auth.remove()
+						Flash.setSuccess('You have successfully logged out!')
+						this.$router.push('/login')
+					}
+				})
+			}
+		}
+	}
+</script>
---
+++ resources/assets/js/components/ImageUpload.vue
@@ -0,0 +1,31 @@
+<template>
+	<div class="image">
+		<image-preview :preview="value" @close="$emit('input', null)" v-if="value"></image-preview>
+		<div class="image__upload" v-else>
+			<input type="file" accept="images/*" @change="upload">
+		</div>
+	</div>
+</template>
+
+<script type="text/javascript">
+	import ImagePreview from './ImagePreview.vue'
+	export default {
+		components: {
+			ImagePreview
+		},
+		props: {
+			value: {
+				type: [String, File],
+				default: null
+			}
+		},
+		methods: {
+			upload(e){
+				const file = e.target.files
+				if (file && file.length > 0) {
+					this.$emit('input', file[0])
+				}
+			}
+		}
+	}
+</script>
